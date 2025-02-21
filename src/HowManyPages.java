a1e9c3baa5e6f4493478713f96f6880495fd65a5
gordon8209181 <gordonz2027@student.cis.edu.hk>
2025-02-21 14:15:33 +0800

    Update HowManyPages.java

--- src/HowManyPages.java
+++ src/HowManyPages.java
@@ -1,8 +1,6 @@
-public class HowManyPages {
-  
-  public static void main(String[] args) {
-    
-    
-  }
-  
-}
+Scanner scanner=new Scanner(System.in);
+System.out.println("Please enter your age");
+int age =scanner.nextInt();
+int num1=100-age;
+System.out.println(age+"years old should read at least"+num1+"pages before giving up on a book");
+            
